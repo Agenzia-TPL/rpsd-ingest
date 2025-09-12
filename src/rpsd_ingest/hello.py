0fefaec161b46c76b46ac896991ad0a7e856787e
caipirginka <github@oicom.com>
2025-09-12 18:00:21 +0200

    Project startup (it contains example code only).

---
+++ src/rpsd_ingest/hello.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+def main() -> None:
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == '__main__':
+    main()
